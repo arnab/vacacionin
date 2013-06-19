29d0d156543828e1968c5e5713a64d1634fa72d9
Arnab Deka <arnab.deka@livingsocial.com>
2013-06-19 16:12:47 +0530

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vacacionin::Application.config.secret_key_base = '56931b7daef2c8fa37e61575ea18897073f1beb5c1a18a29e9b39220b236b0cb839c25f55b3a09a33c6746b9b09dc70aa39f039ed00bb06aaa7ac2aaa782d366'
